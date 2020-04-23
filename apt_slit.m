d7aefd68b15e7d38118d17b873b2ddf46ba93fd4
tkarpish <63946063+tkarpish@users.noreply.github.com>
2020-04-23 15:40:31 -0700

    Add files via upload

---
+++ apt_slit.m
@@ -0,0 +1,31 @@
+%Thomas Karpishin
+%OPT 211
+%April 23, 2020
+%Final Project
+%Making a slit aperture
+
+%Define aperture field
+apl = 5000; %Size of aperture field
+ap = zeros(apl); %Define actual aperture plane
+
+%Define square aperture
+sql = 2500;
+%ap(round(1+apl/2-sql/2):round(1+apl/2+sql/2),round(1+apl/2-sql/2):round(1+apl/2+sql/2)) = 1;
+for ijk = round(1+apl/2-sql/2):round(1+apl/2+sql/2)
+    for lmn = round(1+apl/2-200/2):round(1+apl/2+200/2)
+        ap(ijk,lmn) = 1;
+    end
+end
+
+
+
+%Plot the aperture field
+figure
+imagesc(ap) %Plot image of the aperture field
+colormap gray %set color of the aperture field plot
+axis equal %set the display scale of the axes
+axis([0 apl 0 apl]) %set axes limits to size of aperture field
+
+
+
+
